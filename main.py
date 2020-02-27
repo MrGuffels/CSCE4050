554dc3a144ab71d9c851865b0ce13885a30185d6
Benjamin Guffey <mrben.guffey@gmail.com>
2020-02-27 13:02:43 -0600

    Some Cleaning

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import time
 from utils_demo import *
 
 def main():
-    #file_number = [1,2,3]
-    file_number = [1]
+    file_number = [1,2,3]
     key=-1
     for i in file_number:
         print("File set "+str(i))
@@ -16,15 +15,24 @@ def main():
         message_bytes = string_to_bytes(open('files/m'+str(i)+'.txt',"r").read())
         print (message_bytes)
         #Brute force exhaustive search sub function
-        for i in range(2**24):
-            print (i)
-            bytestring = bitstring_to_bytes(bin(2 ** 127 + i))
+        possible = 2**24
+        start_time = time.time()
+        print ('Total Possible Keys: '+str(possible))
+        percent=possible/100
+        counter = int(percent)+1
+        for j in range(possible):
+            if j == counter:
+                current_time = time.time()
+                print ('Percent Complete: '+str(int((counter/possible)*100)) + '% in ' + str(current_time-start_time) + ' seconds.')
+                counter = counter + int(percent)
+            #print (i)
+            bytestring = bitstring_to_bytes(bin(2 ** 127 + j))
             new_message_bytes = decryptor_CTR(cipher_list,nonce_list,bytestring)
             if new_message_bytes == message_bytes:
-                print("Solution Found:"+str(i))
+                print('File '+ str(i) +'Solution Found:'+str(bin(j)))
                 key=i
                 break
-        if key == -1:
+    if key == -1:
             print ("No Solution Found")
 
 main()
