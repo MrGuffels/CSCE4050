13f77b4feef44bdad3ed5ed58fac5c8be9d65115
Benjamin Guffey <mrben.guffey@gmail.com>
2020-02-27 13:53:17 -0600

    Complete and Working and Clean

--- main.py
+++ main.py
@@ -2,37 +2,55 @@ import sys
 import time
 from utils_demo import *
 
+#Key is 0b110000001100101010111010
+#Takes 5 minutes on average to find.
+
 def main():
-    file_number = [1,2,3]
     key=-1
-    for i in file_number:
-        print("File set "+str(i))
-        #Read cipher#.bin into memory
-        cipher_list = read_bytes('files/c'+str(i)+'.bin')
-        #Read nonce#.bin into memory
-        nonce_list = read_bytes('files/nonce'+str(i)+'.bin')
-        #Read m#.txt into memory
-        message_bytes = string_to_bytes(open('files/m'+str(i)+'.txt',"r").read())
-        print (message_bytes)
-        #Brute force exhaustive search sub function
-        possible = 2**24
-        start_time = time.time()
-        print ('Total Possible Keys: '+str(possible))
-        percent=possible/100
-        counter = int(percent)+1
-        for j in range(possible):
-            if j == counter:
-                current_time = time.time()
-                print ('Percent Complete: '+str(int((counter/possible)*100)) + '% in ' + str(current_time-start_time) + ' seconds.')
-                counter = counter + int(percent)
-            #print (i)
-            bytestring = bitstring_to_bytes(bin(2 ** 127 + j))
-            new_message_bytes = decryptor_CTR(cipher_list,nonce_list,bytestring)
-            if new_message_bytes == message_bytes:
-                print('File '+ str(i) +'Solution Found:'+str(bin(j)))
-                key=i
-                break
+    message_bytes,cipher_list,nonce_list = getFileParts(1)
+    #Brute force exhaustive search sub function
+    possible = 2**24
+    start_time = time.time()
+    print ('Total Possible Keys: '+str(possible))
+    percent=possible/100
+    counter = int(percent)+1
+    for j in range(possible):
+        if j == counter:
+            current_time = time.time()
+            print ('Percent Complete: '+str(int((counter/possible)*100)) + '% in ' + str(current_time-start_time) + ' seconds.')
+            counter = counter + int(percent)
+        bytestring = bitstring_to_bytes(bin(2 ** 127 + j))
+        new_message_bytes = decryptor_CTR(cipher_list,nonce_list,bytestring)
+        if new_message_bytes == message_bytes:
+            print('File 1 Solution Found:'+str(bin(j)))
+            key=j
+            break
     if key == -1:
             print ("No Solution Found")
+    else:
+        bytestring = bitstring_to_bytes(bin(2 ** 127 + key))
+        message_bytes,cipher_list,nonce_list = getFileParts(2)
+        new_message_bytes = decryptor_CTR(cipher_list,nonce_list,bytestring)
+        if new_message_bytes == message_bytes:
+            print('File 2 Solution Found:'+str(bin(j)))
+        else:
+            print("That key doesn't work with File 2")
+        message_bytes,cipher_list,nonce_list = getFileParts(3)
+        new_message_bytes = decryptor_CTR(cipher_list,nonce_list,bytestring)
+        if new_message_bytes == message_bytes:
+            print('File 3 Solution Found:'+str(bin(j)))
+        else:
+            print("That key doesn't work with File 3")
+
+def getFileParts(i):
+    print("File set "+str(i))
+    #Read cipher#.bin into memory
+    cipher_list = read_bytes('files/c'+str(i)+'.bin')
+    #Read nonce#.bin into memory
+    nonce_list = read_bytes('files/nonce'+str(i)+'.bin')
+    #Read m#.txt into memory
+    message_bytes = string_to_bytes(open('files/m'+str(i)+'.txt',"r").read())
+    print (message_bytes)
+    return message_bytes,cipher_list,nonce_list
 
 main()
